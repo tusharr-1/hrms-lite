4e6284c178f9e52df223946a81a16aa489531151
Tushar <tusharchoudhary754@gmail.com>
2026-03-12 02:55:03 +0530

    fix router urls

--- backend/hrms_app/urls.py
+++ backend/hrms_app/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import EmployeeViewSet, AttendanceViewSet, EmployeeAttendanceView
-from . import views
-
 
 router = DefaultRouter()
 
@@ -12,10 +10,4 @@ router.register(r'attendance', AttendanceViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('attendance-history/<int:employee_id>/', EmployeeAttendanceView.as_view()),
-
-    path('employees/', views.EmployeeListCreateView.as_view()),
-    path('employees/<int:pk>/', views.EmployeeDeleteView.as_view()),
-
-    path('attendance/', views.AttendanceCreateView.as_view()),
-    path('attendance-history/<int:emp_id>/', views.AttendanceHistoryView.as_view()),
 ]
