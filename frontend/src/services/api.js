71c9632a54f5665f566457bb73caffe460ffb19f
Tushar <tusharchoudhary754@gmail.com>
2026-03-13 00:43:17 +0530

    fix cors error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://127.0.0.1:8000/api/"
-  baseURL: "https://hrms-backend-z04e.onrender.com/api/"
+  baseURL: "https://hrms-backend-zo4e.onrender.com/api/"
 });
 
 export default API;
