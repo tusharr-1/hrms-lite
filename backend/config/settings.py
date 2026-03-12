f8a2ad0cf0aed8b69f67c9f33cf7c1fef5ddfdb0
Tushar <tusharchoudhary754@gmail.com>
2026-03-13 00:37:22 +0530

    fix cors error

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -45,11 +45,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders', 
+
     'hrms_app',
 
     'rest_framework', 
-
-    'corsheaders', 
 ]
 
 MIDDLEWARE = [
@@ -160,3 +160,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True 
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
